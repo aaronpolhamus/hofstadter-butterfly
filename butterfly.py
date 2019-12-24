75e442935672e9bb5aa65c790246b3d0c447b847
Aaron Polhamus <aaronpolhamus@Aarons-MacBook-Pro.local>
2019-12-24 06:38:50 -0600

    added clumsy try/catch for rare failure cases

--- butterfly.py
+++ butterfly.py
@@ -54,73 +54,75 @@ def get_color(polynew, polyold):
 def pixel_generator(p, q, qmax):
     pi2 = math.pi * 2.0
     maxx = maxy = qmax + 1
+    try:
+        if gcd(p, q) <= 1:
+            sigma = pi2 * p / q
+            nold = 0
+            locations_and_colors = list()
+            for ie in range(0, maxy + 2):
+                e = 8.0 * ie / maxy - 4.0 - 4.0 / maxy
+                n = 0
+                polyold = 1.0
+                poly = 2.0 * math.cos(sigma) - e
+                if polyold * poly < 0.0:
+                    n += 1
 
-    if gcd(p, q) <= 1:
-        sigma = pi2 * p / q
-        nold = 0
-        locations_and_colors = list()
-        for ie in range(0, maxy + 2):
-            e = 8.0 * ie / maxy - 4.0 - 4.0 / maxy
-            n = 0
-            polyold = 1.0
-            poly = 2.0 * math.cos(sigma) - e
-            if polyold * poly < 0.0:
-                n += 1
+                for m in range(2, int(q / 2)):
+                    polynew = (2.0 * math.cos(sigma * m) - e) * poly - polyold
+                    if poly * polynew < 0.0:
+                        n += 1
+                    polyold = poly
+                    poly = polynew
 
-            for m in range(2, int(q / 2)):
-                polynew = (2.0 * math.cos(sigma * m) - e) * poly - polyold
+                polyold = 1.0
+                poly = 2.0 - e
+                if polyold * poly < 0.0:
+                    n += 1
+                polynew = (2.0 * math.cos(sigma) - e) * poly - 2.0 * polyold
                 if poly * polynew < 0.0:
                     n += 1
                 polyold = poly
                 poly = polynew
 
-            polyold = 1.0
-            poly = 2.0 - e
-            if polyold * poly < 0.0:
-                n += 1
-            polynew = (2.0 * math.cos(sigma) - e) * poly - 2.0 * polyold
-            if poly * polynew < 0.0:
-                n += 1
-            polyold = poly
-            poly = polynew
+                for m in range(2, int(q / 2)):
+                    polynew = (2.0 * math.cos(sigma * m) - e) * poly - polyold
+                    if poly * polynew < 0.0:
+                        n += 1
+                    polyold = poly
+                    poly = polynew
 
-            for m in range(2, int(q / 2)):
-                polynew = (2.0 * math.cos(sigma * m) - e) * poly - polyold
+                polynew = (2.0 * math.cos(sigma * q / 2.0) - e) * poly - 2.0 * polyold
+                if poly * polynew < 0.0:
+                    n += 1
+
+                polyold = 1.0
+                poly = 2.0 - e
+                if polyold * poly < 0.0:
+                    n += 1
+                polynew = (2.0 * math.cos(sigma) - e) * poly - 2.0 * polyold
                 if poly * polynew < 0.0:
                     n += 1
                 polyold = poly
                 poly = polynew
 
-            polynew = (2.0 * math.cos(sigma * q / 2.0) - e) * poly - 2.0 * polyold
-            if poly * polynew < 0.0:
-                n += 1
+                for m in range(2, int(q / 2)):
+                    polynew = (2.0 * math.cos(sigma * m) - e) * poly - polyold
+                    if poly * polynew < 0.0:
+                        n += 1
+                    polyold = poly
+                    poly = polynew
 
-            polyold = 1.0
-            poly = 2.0 - e
-            if polyold * poly < 0.0:
-                n += 1
-            polynew = (2.0 * math.cos(sigma) - e) * poly - 2.0 * polyold
-            if poly * polynew < 0.0:
-                n += 1
-            polyold = poly
-            poly = polynew
-
-            for m in range(2, int(q / 2)):
-                polynew = (2.0 * math.cos(sigma * m) - e) * poly - polyold
+                polynew = (2.0 * math.cos(sigma * q / 2.0) - e) * poly - 2.0 * polyold
                 if poly * polynew < 0.0:
                     n += 1
-                polyold = poly
-                poly = polynew
-
-            polynew = (2.0 * math.cos(sigma * q / 2.0) - e) * poly - 2.0 * polyold
-            if poly * polynew < 0.0:
-                n += 1
-            if n > nold:
-                locations_and_colors.append((int(maxy - ie), int(maxx * p / q), get_color(polynew, polyold)))
-                locations_and_colors.append((int(maxx * p / q), int(maxy - ie), get_color(polynew, polyold)))
-            nold = n
-        return locations_and_colors
-    return None
+                if n > nold:
+                    locations_and_colors.append((int(maxy - ie), int(maxx * p / q), get_color(polynew, polyold)))
+                    locations_and_colors.append((int(maxx * p / q), int(maxy - ie), get_color(polynew, polyold)))
+                nold = n
+            return locations_and_colors
+        return None
+    except:
+        return None
 
 
 if __name__ == '__main__':
